4cfe5b7692bd1f1929f3eca6320fd4d2a56738ac
HuYihe2008 <2812856215@qq.com>
2022-08-10 18:43:13 +0800

    新增欢迎内容 ”不可编辑“

--- nonebot_plugin_admin/__init__.py
+++ nonebot_plugin_admin/__init__.py
@@ -38,6 +38,18 @@ from .utils import At, Reply, MsgText, banSb, init, check_func_status, change_s_
 from .group_request_verify import verify
 from .config import plugin_config, global_config
 
+import random
+from asyncio import sleep
+from pathlib import Path
+
+from nonebot import get_driver, on_request, on_notice
+from nonebot.adapters.onebot.v11 import Bot, FriendRequestEvent, GroupRequestEvent, GroupIncreaseNoticeEvent, \
+    MessageSegment, Message, FriendAddNoticeEvent, HonorNotifyEvent
+
+from .utils.config import config
+from .utils.message_util import MessageBuild
+
+
 su = global_config.superusers
 cb_notice = plugin_config.callback_notice
 
@@ -445,6 +457,26 @@ async def _(bot: Bot, event: NoticeEvent):
     await bot.send_group_msg(group_id=group_id, message=recall_notice + recalled_message['message'])
     await group_recall.finish()
 
+#群欢迎部分
+
+
+superuser = int(list(get_driver().config.superusers)[0])
+
+requests_handle = on_request(priority=5, block=True)
+notice_handle = on_notice(priority=5, block=True)
+
+
+@notice_handle.handle()
+async def GroupNewMember(bot: Bot, event: GroupIncreaseNoticeEvent):
+    greet_emoticon = MessageBuild.Image(Path() / 'resources' / 'LittlePaimon' / 'emoticons' / '派蒙-干杯.png', mode='RGBA')
+    if event.user_id == event.self_id:
+        await sleep(random.randint(4, 8))
+        await bot.send_group_msg(group_id=event.group_id, message=Message(
+            MessageSegment.text('旅行者们大家好呀~，这里是小派蒙，对我说help查看帮助吧~\n')))
+    elif event.group_id not in config.paimon_greet_ban:
+        await sleep(random.randint(4, 8))
+        await bot.send_group_msg(group_id=event.group_id, message=Message(
+            MessageSegment.at(event.user_id) + MessageSegment.text("欢迎新旅行者哦~\n")))
 
 __usage__ = """
 【初始化】：
