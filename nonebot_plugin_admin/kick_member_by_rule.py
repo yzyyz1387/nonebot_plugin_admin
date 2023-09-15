bc6e79f7823dcf7413568d27c51829fed91f34bb
yzyyz1387 <1796031384@qq.com>
2023-09-16 04:01:46 +0800

    :bug: 修复bug

--- nonebot_plugin_admin/kick_member_by_rule.py
+++ nonebot_plugin_admin/kick_member_by_rule.py
@@ -9,21 +9,48 @@ import asyncio
 import datetime
 from random import randint
 
-from nonebot import on_command, logger, require
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, Message, MessageSegment, MessageEvent
+from nonebot import on_command, logger
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 from nonebot.adapters.onebot.v11.exception import ActionFailed
-from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
-from nonebot.matcher import Matcher
-from nonebot.params import ArgPlainText, ArgStr, CommandArg
-
-from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11.permission import GROUP_OWNER
+from nonebot.params import ArgStr
 from nonebot.permission import SUPERUSER
-from .admin_role import DEPUTY_ADMIN
+from nonebot.typing import T_State
 
 from .config import global_config
-from .utils import At, MsgText, banSb, change_s_title, fi, log_fi, sd, Reply, log_sd
 
 su = global_config.superusers
+global level_dic, last_send_dict
+
+
+def make_send(kick_list: list, category: str, data_dict: dict):
+    """
+    :param kick_list: 踢出列表
+    :param category: 踢出条件
+    :param data_dict: 踢出列表对应的数据字典
+    :return: 生成的发送文本
+    根据踢出列表和踢出条件生成发送文本
+    """
+    kick_c_index = int(category) - 1
+    prompt = [["将踢出等级 ≤", " 的成员:\n", "等级:"], ["将踢出在 ", "之后未发言的成员:\n", "最后："]]
+    send_text = prompt[kick_c_index][0] + category + prompt[kick_c_index][1]
+    if kick_list:
+        for qq in kick_list:
+            send_text += f"{qq}： {prompt[kick_c_index][2]}{ data_dict[qq] if category == '1' else datetime.datetime.fromtimestamp(data_dict[qq])}\n"
+    else:
+        send_text += "没有满足条件的成员, 已取消操作..."
+    return send_text
+
+
+async def get_qq_lever(bot: Bot, qq: int):
+    """
+    :param bot: bot
+    :param qq: qq号
+    :return: qq等级
+    获取qq等级
+    """
+    return (await bot.get_stranger_info(user_id=qq, no_cache=True))['level']
+
 
 kick_by_rule = on_command('成员清理', priority=1, block=True,
                           permission=SUPERUSER | GROUP_OWNER)
@@ -46,17 +73,18 @@ async def _(
         await kick_by_rule.reject("输入错误, 请重新输入:")
 
 
-@kick_by_rule.got('k_level', prompt='请输入:')
+@kick_by_rule.got('kick_condition', prompt='请输入:')
 async def _(
         bot: Bot,
         event: GroupMessageEvent,
         state: T_State,
-        k_level=ArgStr()
+        kick_condition=ArgStr()
 ):
+    global level_dic, last_send_dict
     await kick_by_rule.send("请坐和放宽，轮询成员信息中，这可能会花费几分钟...")
-    k_level = str(k_level)
+    kick_condition = str(kick_condition)
     kick_list = []
-    if k_level in ["取消", "算了"]:
+    if kick_condition in ["取消", "算了"]:
         await kick_by_rule.finish("已取消操作...")
     else:
         member_list = await bot.get_group_member_list(group_id=event.group_id)
@@ -68,7 +96,7 @@ async def _(
             for qq in qq_list:
                 level_dic[qq] = (await get_qq_lever(bot, qq))
 
-            kick_list = [qq for qq, level in level_dic.items() if 0 < level <= int(k_level)]
+            kick_list = [qq for qq, level in level_dic.items() if 0 < level <= int(kick_condition)]
             zero_level_list = [qq for qq, level in level_dic.items() if level == 0]
             state['zero_level_list'] = zero_level_list
             send_0_tips = f"0级成员：\n"
@@ -78,35 +106,28 @@ async def _(
                 send_0_tips += "\n0级成员可能是未获取到等级信息，不做处理\n"
                 await kick_by_rule.send(send_0_tips)
 
-            # for qq, level in level_dic.items():
-            #     logger.info(f"{qq}:{level}级")
-            #     if level <= int(k_level):
-            #         kick_list.append(qq)
-            #         logger.info(qq <= level)
-
-            send_text = f"将踢出等级 ≤ {k_level} 的成员:\n"
-            if kick_list:
-                for qq in kick_list:
-                    send_text += f"【{qq}】 【{level_dic[qq]}级】\n"
-                await kick_by_rule.send(send_text)
         elif category == "2":
-            last_send_list = {}
-            k_level = str(k_level)
+            last_send_dict = {}
+            kick_condition = str(kick_condition)
             for member in member_list:
-                last_send_list[member['user_id']] = member['last_sent_time']
-            # logger.info(f"last_send_list: {last_send_list}")
+                last_send_dict[member['user_id']] = member['last_sent_time']
+            logger.debug(f"last_send_list: {last_send_dict}")
             try:
-                if len(k_level) != 8:
+                if len(kick_condition) != 8:
                     raise ValueError
-                input_time = datetime.datetime.strptime(k_level, "%Y%m%d")
+                input_time = datetime.datetime.strptime(kick_condition, "%Y%m%d")
                 now_time = datetime.datetime.now()
                 if input_time > now_time:
                     await kick_by_rule.reject("日期不能大于当前日期, 请重新输入:")
-                for qq, last in last_send_list.items():
+                for qq, last in last_send_dict.items():
                     try:
-                        if datetime.datetime.fromtimestamp(last) < input_time:
+                        logger.debug(f"{qq}：{datetime.datetime.fromtimestamp(last)}")
+                        logger.debug(f"{datetime.datetime.fromtimestamp(last) <= input_time}")
+                        if datetime.datetime.fromtimestamp(last) <= input_time:
+
                             kick_list.append(qq)
-                    except ValueError:
+                    except ValueError as e:
+                        logger.error(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]{qq}：{e}")
                         pass
             except ValueError:
                 await kick_by_rule.finish("日期格式错误")
@@ -115,9 +136,16 @@ async def _(
         if kick_list:
             # 保存踢出列表
             state['kick_list'] = kick_list
-            # logger.info(f"kick_list: {kick_list}")
+            logger.debug(f"kick_list: {kick_list}")
             if len(member_list) - len(kick_list) <= 3:
                 await kick_by_rule.finish("踢出后群人数将少于3人, 已取消操作...")
+            else:
+                await kick_by_rule.send(
+                    make_send(
+                        kick_list,
+                        category,
+                        level_dic if category == "1" else last_send_dict
+                    ))
         else:
             await kick_by_rule.finish("没有满足条件的成员, 已取消操作...")
 
@@ -126,7 +154,7 @@ async def _(
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     confirm = str(state['confirm'])
     if confirm == "1":
-        await kick_by_rule.send("正在踢出...")
+        await kick_by_rule.send("正准备执行...")
     else:
         await kick_by_rule.finish("已取消操作...")
 
@@ -142,10 +170,11 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
             try:
                 await asyncio.sleep(randint(0, 5))  # 睡眠随机时间，避免黑号
                 await bot.set_group_kick(group_id=event.group_id, user_id=qq)
+                logger.debug(f"踢出{qq},操作者：{event.user_id},群：{event.group_id}")
                 # await bot.send_group_msg(group_id=event.group_id, message=f"T{qq}") # 测试用
                 success.append(qq)
             except ActionFailed as e:
-                logger.error(f"{qq}-踢出失败")
+                logger.error(f"{qq}-踢出失败：{e}")
                 fail.append(qq)
         if success:
             await kick_by_rule.send(f"踢出成功：{success}")
@@ -156,10 +185,8 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
         await kick_by_rule.send("没有需要踢出的成员, 已取消操作...")
 
 
-get_by_qq = on_command("/get", priority=1, permission=GROUP_OWNER | SUPERUSER)
-
-
 # 测试
+# get_by_qq = on_command("/get", priority=1, permission=GROUP_OWNER | SUPERUSER)
 # @get_by_qq.handle()
 # async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
 #     if qq := args.extract_plain_text():
@@ -167,7 +194,3 @@ get_by_qq = on_command("/get", priority=1, permission=GROUP_OWNER | SUPERUSER)
 #         await get_by_qq.finish(str(info))
 #     else:
 #         await get_by_qq.finish("PSE INPUT ARG")
-
-
-async def get_qq_lever(bot: Bot, qq: int):
-    return (await bot.get_stranger_info(user_id=qq, no_cache=True))['level']
