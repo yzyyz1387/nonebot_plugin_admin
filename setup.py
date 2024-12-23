84d07a871d36ca1cd8d882806dfd1a6c6e620e5a
幼稚园园长 <1796031384@qq.com>
2024-12-23 08:17:31 +0800

    v0.4.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-admin",
-    version="0.4.5.5",
+    version="0.4.6",
     author="yzyyz1387",
     author_email="youzyyz1384@qq.com",
     keywords=("pip", "nonebot2", "nonebot", "admin", "nonebot_plugin"),
