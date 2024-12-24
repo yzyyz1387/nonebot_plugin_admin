f5c00292b7713d2a86f4dcc7f8ba692e150e7974
幼稚园园长 <1796031384@qq.com>
2024-12-24 17:14:02 +0800

    fix：file_util等包导入错误

---
+++ nonebot_plugin_admin/util/__init__.py
@@ -0,0 +1,10 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2024/12/24 17:12
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from .file_util import *
+from .time_util import *
