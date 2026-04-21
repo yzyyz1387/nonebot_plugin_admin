eea394099502be9093dc4aedf79a6b3ae01702d5
幼稚园园长 <1796031384@qq.com>
2026-04-22 05:20:29 +0800

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="nonebot-plugin-admin",
-    version="1.0.0",
+    version="1.0.1",
     author="yzyyz1387",
     author_email="youzyyz1384@qq.com",
     keywords=["pip", "nonebot2", "nonebot", "admin", "nonebot_plugin"],
